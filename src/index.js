f46f84df529fdb351edafedd239b7a47d9858c11
BachPham <bach.pham@zien.com>
2023-07-09 22:34:03 +0700

    base code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const express = require("express");
+var morgan = require("morgan");
+const handlebars = require("express-handlebars").engine;
+
+const port = 3000;
+const app = express();
+
+const route = require("./routes");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//HTTP Logger
+app.use(morgan("combined"));
+//Template Engine
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+// Home, search, contact
+
+//Route init
+route(app);
+
+app.listen(port);
